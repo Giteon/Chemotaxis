2458d5cf436c430c908371a9add58ad14ebc09d4
unknown <safox@697-334-eowyn.sf.k12.internal>
2014-09-26 11:14:35 -0700

    gideon is phat

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria [] spore;
-void setup()
+public void setup()
 {
 	size(1000, 700);
 	spore = new Bacteria[200];
@@ -10,7 +26,7 @@ void setup()
 		spore[i] = new Bacteria();
 	}
 }
-void draw()   
+public void draw()   
 {   
 	fill(0, 0, 0, 30);
 	rect(0, 0, 1000, 700);
@@ -29,7 +45,7 @@ void draw()
 				if (spore[i].mySize < spore[r].mySize)
 				{
 					spore[r].myColor += (int)(Math.random()*8-4); /*= (spore[r].myColor+spore[i].myColor)/2;*/
-					spore[r].mySize += (.4) * spore[i].mySize;
+					spore[r].mySize += (.4f) * spore[i].mySize;
 					spore[r].myNum +=1;
 					spore[i].mySize = 0;
 					spore[i].myX = 9999;
@@ -42,7 +58,7 @@ void draw()
 					if (spore[r].mySize < spore[i].mySize)
 					{
 						spore[i].myColor += (int)(Math.random()*8-4); /*= (spore[r].myColor+spore[i].myColor)/2;*/
-						spore[i].mySize += (.8) * spore[r].mySize;
+						spore[i].mySize += (.8f) * spore[r].mySize;
 						spore[i].myNum +=1;
 						spore[r].mySize = 0;
 						spore[r].myX = 9999;
@@ -69,7 +85,7 @@ class Bacteria
 		myX = (int)(Math.random()*980);
 		myNum = (int)(mySize*55+(int)(Math.random()*9));
 	}
-	void show()
+	public void show()
 	{
 		fill(myColor);
 		myColor = myColor + (int)(Math.random()*6-3);
@@ -80,11 +96,11 @@ class Bacteria
 		textSize(mySize/2);
 		if (myNum < 99)
 		{
-			text(myNum, myX-mySize/3.2, myY+mySize/5);
+			text(myNum, myX-mySize/3.2f, myY+mySize/5);
 		}
 		else
 		{
-			text(myNum, myX-mySize/2.13, myY+mySize/5);
+			text(myNum, myX-mySize/2.13f, myY+mySize/5);
 
 		}
 		popStyle();
@@ -94,7 +110,7 @@ class Bacteria
 			myColor = 254;
 		}
 	}
-	void move()
+	public void move()
 	{
 		boolean mouseDown = false;
 		if (mousePressed == true)
@@ -109,35 +125,35 @@ class Bacteria
 		{
 			if (mouseX+mySize/2>myX)
 			{
-				myX = myX + (int)(Math.random()*8-3.82);
+				myX = myX + (int)(Math.random()*8-3.82f);
 			}
 			if (mouseY+mySize/2>myY)
 			{
-				myY = myY + (int)(Math.random()*8-3.82);
+				myY = myY + (int)(Math.random()*8-3.82f);
 			}
 			if (mouseX-mySize/2<myX)
 			{
-				myX = myX + (int)(Math.random()*8-4.22);
+				myX = myX + (int)(Math.random()*8-4.22f);
 			}
 			if (mouseY-mySize/2<myY)
 			{
-				myY = myY + (int)(Math.random()*8-4.22);
+				myY = myY + (int)(Math.random()*8-4.22f);
 			}
 			if (myX>1000)
 			{
-				myX = myX - (int)(Math.random()*8-4.1);
+				myX = myX - (int)(Math.random()*8-4.1f);
 			}
 			if (myY>700)
 			{
-				myY = myY - (int)(Math.random()*8-4.1);
+				myY = myY - (int)(Math.random()*8-4.1f);
 			}
 			if (myX<0)
 			{
-				myX = myX - (int)(Math.random()*8-3.9);
+				myX = myX - (int)(Math.random()*8-3.9f);
 			}
 			if (myY<0)
 			{
-				myY = myY - (int)(Math.random()*8-3.9);
+				myY = myY - (int)(Math.random()*8-3.9f);
 			}
 		}
 		if (mousePressed == true && (mouseX >= myX-mySize/3 && mouseY >= myY-mySize/3 && mouseX <= (myX+ mySize+mySize/3) && mouseY <= (myY+ mySize+mySize/3)))
@@ -152,3 +168,12 @@ class Bacteria
 		}
 	}
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
